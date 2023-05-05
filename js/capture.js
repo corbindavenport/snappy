4599c4246defb5cb6af8e3de83d6ee666c8777c0
Corbin Davenport <me@corbin.io>
2023-05-05 19:12:35 -0400

    Clean up code, initial support for timed capture

--- js/capture.js
+++ js/capture.js
@@ -1,14 +1,23 @@
 const videoEl = document.getElementById('capture-video');
+var captureStream = null;
 var activeFolder = null;
+var captureInterval = null;
 
 console.log('List of supported capture options:', navigator.mediaDevices.getSupportedConstraints());
 
-document.getElementById('capture-select-btn').addEventListener('click', async function () {
+function checkIfReady() {
+    if (activeFolder && captureStream.active) {
+        document.getElementById('capture-btn').removeAttribute('disabled')
+    } else {
+        document.getElementById('capture-btn').setAttribute('disabled', 'true')
+    }
+}
+
+async function selectCaptureTarget() {
     var constraints = {
         audio: false,
         video: {
             frameRate: 1,
-            contrast: 100,
             cursor: 'never'
         },
         selfBrowserSurface: 'exclude'
@@ -17,17 +26,24 @@ document.getElementById('capture-select-btn').addEventListener('click', async fu
     if (!captureStream) {
         alert('There was an error!');
     }
+    // Handle capture stop
+    captureStream.addEventListener('inactive', function() {
+        console.log('Capture stopped');
+        document.getElementById('capture-select-status').innerText = 'No capture target selected';
+        checkIfReady();
+    }, { once: true });
     videoEl.srcObject = captureStream;
+    document.getElementById('capture-select-status').innerText = captureStream.id;
     videoEl.play();
-    console.log(captureStream);
-});
+    // Check if capture is ready
+    checkIfReady();
+}
 
-// TODO: No UI for saveFile, start button should work as start and stop
-
-function saveFile() {
-    var fileName = 'test';
+function captureScreenshot() {
     var fileQuality = 0.95;
     var imgFormat = document.getElementById('capture-img-format').value;
+    var intervalTime = (parseFloat(document.getElementById('capture-interval').value) * 1000);
+    console.log(intervalTime)
     // Set file ending
     if (imgFormat === 'image/jpeg') {
         var fileEnding = '.jpg';
@@ -36,6 +52,17 @@ function saveFile() {
     } else if (imgFormat === 'image/webp') {
         var fileEnding = '.webp';
     }
+    // Save first file
+    saveToDisk(fileEnding, imgFormat, fileQuality)
+    // Start capture on loop
+    captureInterval = setInterval(saveToDisk, intervalTime, fileEnding, imgFormat, fileQuality);
+}
+
+async function saveToDisk(fileEnding, imgFormat, fileQuality) {
+    // Get date
+    var date = new Date();
+    var fileName = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDate() + '-' + date.getHours() + '-' + date.getMinutes() + '-' + date.getMilliseconds();
+    // Capture image
     var canvas = document.getElementById('capture-canvas');
     canvas.width = videoEl.videoWidth;
     canvas.height = videoEl.videoHeight;
@@ -54,11 +81,25 @@ function saveFile() {
     }, imgFormat, fileQuality);
 }
 
-document.getElementById('folder-btn').addEventListener('click', async function () {
-    // Open file picker and destructure the result the first handle
+async function selectFolder() {
     activeFolder = await window.showDirectoryPicker({
         'mode': 'readwrite'
     });
-    document.getElementById('folder-name').value = activeFolder.name;
-    console.log(activeFolder);
+    document.getElementById('folder-select-status').innerText = 'Folder: ' + activeFolder.name;
+    // Check if capture is ready
+    checkIfReady();
+}
+
+// Main buttons
+
+document.getElementById('folder-select-btn').addEventListener('click', function () {
+    selectFolder();
+});
+
+document.getElementById('capture-select-btn').addEventListener('click', async function () {
+    selectCaptureTarget();
+});
+
+document.getElementById('capture-btn').addEventListener('click', async function () {
+    captureScreenshot();
 });
